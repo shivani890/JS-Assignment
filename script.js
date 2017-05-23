569de106094b6cd3f7fd0e1e76bb04f92c69592d
Brahmjeet <brahmjeet@peppersquare.com>
2017-05-23 17:05:13 +0530

    TOdoApp

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function newActivity() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("toDoList").appendChild(li);
+  }
+} 
